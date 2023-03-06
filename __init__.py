311851df075baee8d352676e76a7d507aad9b9c5
JayReigns <jayantasxiomi@gmail.com>
2023-03-06 20:15:42 +0530

    Added History Menu

--- __init__.py
+++ __init__.py
@@ -2,14 +2,22 @@
 bl_info = {
     "name": "API Browser",
     "author": "JayReigns",
-    "version": (1, 2, 3),
+    "version": (1, 3, 1),
     "blender": (2, 80, 0),
     "location": "Text Editor > ToolBar > API Browser",
     "description": "Browse through the python api via the user interface",
     "category": "Development"
 }
 
-DEFAULT_MODULE = 'bpy'
+import bpy
+from bpy.props import IntProperty, StringProperty, BoolProperty, PointerProperty
+from bpy.types import Operator, Menu, Panel, PropertyGroup, AddonPreferences
+
+try:  # if bpy.app.version < (3, 3, 0):
+    from console.complete_import import get_root_modules
+except:  # else:
+    from bl_console_utils.autocomplete.complete_import import get_root_modules
+
 
 # category icons
 ICONS = ["TRIA_DOWN", "TRIA_DOWN", "PACKAGE", "WORDWRAP_ON",
@@ -19,14 +27,18 @@ ICONS = ["TRIA_DOWN", "TRIA_DOWN", "PACKAGE", "WORDWRAP_ON",
 LABELS = ["Items", "Values", "Modules", "Types", "Properties",
           "Structs and Functions", "Methods and Functions", "Attributes", "Inaccessible"]
 
+GOTO_PARENT = -1
+GOTO_DEFAULT = -2
+GOTO_HISTORY = -3
 
-import bpy
+# class Dummy:
+#     columns = 3
+#     default_module = 'bpy'
+#     history_size = 3
 
-try:  # if bpy.app.version < (3, 3, 0):
-    from console.complete_import import get_root_modules
-except:  # else:
-    from bl_console_utils.autocomplete.complete_import import get_root_modules
-
+def get_preferences(context):
+    return context.preferences.addons[__name__].preferences
+    # return Dummy
 
 def resolve_module(path):
     """Returns Python Object from String Path"""
@@ -37,50 +49,87 @@ def resolve_module(path):
         return None
 
 
-def resolve_path(cur_path, info):
+def parent(path):
+    """Returns the parent path"""
+    return path.rpartition('[' if path.endswith(']') else '.')[0]
 
-    def parent(path):
-        """Returns the parent path"""
-        return path.rpartition('[' if path.endswith(']') else '.')[0]
 
-    # category_index, item_index
+def resolve_path(cur_path, info, default=""):
+    """Returns tuple(path, filter)"""
+
+    filter = ""
     type, value = (int(i) for i in info.split())
 
-    if type == -1:
+    if type >= 0:
+        if   type == 0: # key
+            cur_path += f"['{API_Manager.get_data_tree()[type][value]}']"
+
+        elif type == 1: # index
+            cur_path += f"[{value}]", ""
+
+        else:
+            if cur_path:
+                cur_path += '.'
+            cur_path += API_Manager.get_data_tree()[type][value]
+    
+    elif type == GOTO_PARENT:
         cur_path = parent(cur_path)
-    elif type == -2:
-        cur_path = DEFAULT_MODULE
-    elif type == 0:  # index
-        cur_path += f"['{API_Manager.filtered_tree[type][value]}']"
-    elif type == 1:  # key
-        cur_path += f"[{value}]"
-    else:
-        if cur_path:
-            cur_path += '.'
-        cur_path += API_Manager.filtered_tree[type][value]
 
-    return cur_path
+    elif type == GOTO_DEFAULT:
+        cur_path = default
 
+    elif type == GOTO_HISTORY:
+        hist = API_Manager.get_history()[value]
+        cur_path = hist[0]
+        filter = hist[1]
+    
+    return cur_path, filter
+
+
+def get_module_description(path):
+    module = resolve_module(path)
+    desc = str(module)
+    doc = str(module.__doc__)
+
+    if doc != "None":
+        # omit last '.'; blender adds an '.' after
+        doc = doc.rstrip()
+        if doc.endswith("."):
+            doc = doc[:-1]    
+        desc += "\n\n" + doc
+    
+    return desc
 
 class API_Manager:
-
-    # used internally
-    _data_tree = None
 
     # used to check for changes
     _current_path = None
     _current_filter = None
     _filter_internal = None
 
-    # public
-    filtered_tree = None
-    current_module = None
+    # used internally
+    _data_tree = None
+    _filtered_tree = None
+    _current_module = None
+    _history = []
 
-    def update(forced=False):
+    def get_current_path():
+        return API_Manager._current_path
+    
+    def get_current_module():
+        return API_Manager._current_module
+    
+    def get_history():
+        return API_Manager._history
+    
+    def get_data_tree():
+        return API_Manager._filtered_tree
+
+    def check_for_update():
         api_prop = bpy.context.window_manager.api_props
 
-        if forced or API_Manager._current_path != api_prop.path:
-            API_Manager._update_path()
+        if API_Manager._current_path != api_prop.path:
+            API_Manager.update_path()
 
         elif API_Manager._current_filter != api_prop.filter or API_Manager._filter_internal != api_prop.filter_internal:
             API_Manager._update_filter()
@@ -103,66 +152,120 @@ class API_Manager:
 
         API_Manager._current_filter = filter_text
         API_Manager._filter_internal = filter_internal
-        API_Manager.filtered_tree = tree
+        API_Manager._filtered_tree = tree
 
-    def _update_path():
 
-        def categorize_module(module):
+    def _update_history(new_path):
 
-            def isiterable(mod):
-                try:
-                    # (str, byte) can be passed but bpy.app gets ignored
-                    return not isinstance(mod, str) and iter(mod)
-                except:
-                    return False
+        def remove_if_exists(hist, exclude):
+            return [(p,f) for p, f in hist if p not in exclude]
 
-            itm, val, mod, typ, props, struct, met, att, bug = [], [], [], [], [], [], [], [], []
+        old_path = API_Manager._current_path
+        filter = API_Manager._current_filter
 
-            if isiterable(module):
-                if hasattr(module, 'keys'):
-                    itm = [str(k) for k in list(module.keys())]
+        # remove new_path and old_path if exists
+        API_Manager._history = remove_if_exists(API_Manager._history, (new_path, old_path))
+        
+        if not old_path:
+            return
+        
+        history = API_Manager._history
+        history_size = get_preferences(bpy.context).history_size
 
-                if not itm:
-                    val = [str(v) for v in list(module)]
+        # append old_path
+        history.append((old_path, filter))
 
-            for i in dir(module):
-                try:
-                    t = str(type(eval(f"module.{i}", {'module': module})))
-                except:
-                    bug += [i]
-                    continue
+        if len(history) > history_size:
+            additional = len(history) - history_size
+            del history[:additional]
 
-                if t == "<class 'module'>":
-                    mod += [i]
-                elif t.startswith("<class 'bpy_prop"):
-                    props += [i]
-                elif t.startswith("<class 'bpy"):
-                    struct += [i]
-                elif t == "<class 'builtin_function_or_method'>":
-                    met += [i]
-                elif t == "<class 'type'>":
-                    typ += [i]
-                else:
-                    att += [i]
 
-            return [itm, val, mod, typ, props, struct, met, att, bug]
+    def update_path():
 
         path = bpy.context.window_manager.api_props.path
         module = resolve_module(path)
 
         if path:
-            tree = categorize_module(module)
+            tree = API_Manager.categorize_module(module)
         else:
             tree = [[], [], get_root_modules(), [], [], [], [], [], []]
 
-        API_Manager.current_module = module
+        API_Manager._update_history(path)
+
+        API_Manager._current_module = module
         API_Manager._current_path = path
         API_Manager._data_tree = tree
+
         API_Manager._update_filter()
 
+    
+    def categorize_module(module):
+
+        def isiterable(mod):
+            try:
+                # (str, byte) can be passed but bpy.app gets ignored
+                return not isinstance(mod, str) and iter(mod)
+            except:
+                return False
+
+        itm, val, mod, typ, props, struct, met, att, bug = [], [], [], [], [], [], [], [], []
+
+        if isiterable(module):
+            if hasattr(module, 'keys'):
+                itm = [str(k) for k in list(module.keys())]
+
+            if not itm:
+                val = [str(v) for v in list(module)]
+
+        for i in dir(module):
+            try:
+                t = str(type(eval(f"module.{i}", {'module': module})))
+            except:
+                bug += [i]
+                continue
+
+            if t == "<class 'module'>":
+                mod += [i]
+            elif t.startswith("<class 'bpy_prop"):
+                props += [i]
+            elif t.startswith("<class 'bpy"):
+                struct += [i]
+            elif t == "<class 'builtin_function_or_method'>":
+                met += [i]
+            elif t == "<class 'type'>":
+                typ += [i]
+            else:
+                att += [i]
+
+        return [itm, val, mod, typ, props, struct, met, att, bug]
+
+#########################################################################################
+# OPERATORS, MENUS
 #########################################################################################
 
-class API_OT_GOTO_Module(bpy.types.Operator):
+
+class API_MT_History_Menu(Menu):
+    bl_label = "History"
+    bl_idname = "API_MT_History_Menu"
+
+    def draw(self, context):
+        layout = self.layout
+        length = len(API_Manager.get_history()) - 1
+        for i, item in enumerate(reversed(API_Manager.get_history())):
+            layout.operator(API_OT_GOTO_Module.bl_idname, 
+                            text=item[0]).info = f'{GOTO_HISTORY} {length - i}'
+
+
+class API_OT_History(Operator):
+    bl_label = "History"
+    bl_idname = "api_browser.history"
+
+    def execute(self, context):
+        bpy.ops.wm.call_menu(name=API_MT_History_Menu.bl_idname)
+        return {'FINISHED'}
+
+
+class API_OT_GOTO_Module(Operator):
     """Go to this Module"""
     bl_idname = "api_browser.goto_module"
     bl_label = "Go To Module"
@@ -170,54 +273,44 @@ class API_OT_GOTO_Module(bpy.types.Operator):
     # category_index, item_index; ex: "1 2"
     info: bpy.props.StringProperty(name="info", default="")
 
+
     @classmethod
     def description(cls, context, properties):
         # provides live description
-
+        prefs = get_preferences(context)
         api_prop = context.window_manager.api_props
-        type, value = (int(i) for i in properties.info.split())
 
-        if type >= 0:
-            module = resolve_module(resolve_path(api_prop.path, properties.info))
-            desc = str(module)
-            doc = str(module.__doc__)
+        path, _filter = resolve_path(api_prop.path, properties.info, default=prefs.default_module)
 
-            if doc != "None":
-                # omit last '.'; blender adds an '.' after
-                doc = doc.rstrip()
-                if doc.endswith("."):
-                    doc = doc[:-1]    
-                desc += "\n\n" + doc
-            
-            return desc
-
-        elif type == -1:
-            return "Go to Parent Module"
-        elif type == -2:
-            return "Go to 'bpy' Module"
-
+        if path:
+            return get_module_description(path)
+        
 
     def execute(self, context):
 
+        prefs = get_preferences(context)
         api_prop = context.window_manager.api_props
-        api_prop.filter = ""
-        api_prop.path = resolve_path(api_prop.path, self.info)
 
+        path, filter = resolve_path(api_prop.path, self.info, default=prefs.default_module)
+
+        api_prop.path = path
+        api_prop.filter = filter
+        
         return {'FINISHED'}
 
 
-class API_OT_Reload_Module(bpy.types.Operator):
+class API_OT_Reload_Module(Operator):
     """Reloads the Current module"""
     bl_idname = "api_browser.reload"
     bl_label = "Reload"
 
     def execute(self, context):
-        API_Manager.update(forced=True)
+        API_Manager.update_path()
         self.report({"INFO"}, "Module Reloaded!")
         return {'FINISHED'}
 
 
-class API_OT_Copy_Text(bpy.types.Operator):
+class API_OT_Copy_Text(Operator):
     """Copy Text"""
     bl_idname = "api_browser.copy_text"
     bl_label = "Copy"
@@ -230,7 +323,7 @@ class API_OT_Copy_Text(bpy.types.Operator):
         return {'FINISHED'}
 
 
-class API_OT_Api_Info(bpy.types.Operator):
+class API_OT_Api_Info(Operator):
     """Show API Info"""
     bl_idname = "api_browser.api_info"
     bl_label = "Info"
@@ -244,7 +337,7 @@ class API_OT_Api_Info(bpy.types.Operator):
     def draw(self, context):
         path = context.window_manager.api_props.path
 
-        current_module = API_Manager.current_module
+        current_module = API_Manager.get_current_module()
         layout = self.layout
 
         def draw_text(text, layout):
@@ -269,24 +362,24 @@ class API_OT_Api_Info(bpy.types.Operator):
         draw_section(layout, label="Doc:", text=str(current_module.__doc__), icon="INFO", newline=True)
 
 #########################################################################################
+# PANELS
+#########################################################################################
 
-class API_PT_Browser(bpy.types.Panel):
-    bl_idname = "API_PT_api_browser"
+class API_PT_Browser(Panel):
+    bl_idname = "API_PT_Browser"
     bl_space_type = "TEXT_EDITOR"
     bl_region_type = "UI"
     bl_label = "API Browser"
     bl_options = {'DEFAULT_CLOSED'}
     bl_category = "Text"
 
-    columns = 3
-
     def draw(self, context):
+        API_Manager.check_for_update()
 
+        prefs = get_preferences(context)
         api_prop = context.window_manager.api_props
-        API_Manager.update()
 
         layout = self.layout
-
         col = layout.column(align=True)
 
         # path bar
@@ -300,9 +393,12 @@ class API_PT_Browser(bpy.types.Panel):
         # operators
         row = col.row(align=True)
         row.operator(API_OT_GOTO_Module.bl_idname,
-                     text="Parent", icon="BACK").info = '-1 0'
-        row.operator(API_OT_GOTO_Module.bl_idname, text='bpy',
-                     emboss=True, icon="FILE_PARENT").info = '-2 0'
+                     text="Parent", icon="BACK").info = f'{GOTO_PARENT} 0'
+        row.operator(API_OT_GOTO_Module.bl_idname, text=prefs.default_module,
+                     emboss=True, icon="FILE_PARENT").info = f'{GOTO_DEFAULT} 0'
+
+        # history
+        row.operator(API_OT_History.bl_idname, text="", icon="RECOVER_LAST")
 
         # search bar
         row = layout.row(align=True)
@@ -310,7 +406,8 @@ class API_PT_Browser(bpy.types.Panel):
         row.prop(api_prop, "filter_internal",
                  icon='FILTER', text="", toggle=True)
 
-        data_tree = API_Manager.filtered_tree
+        data_tree = API_Manager.get_data_tree()
+        columns = prefs.columns
 
         for i, category in enumerate(data_tree):
             if not category:
@@ -325,23 +422,55 @@ class API_PT_Browser(bpy.types.Panel):
             # items
             col = box.column(align=True)
             for j, entry in enumerate(category):
-                if not (j % self.columns):
+                if not (j % columns):
                     row = col.row(align=True)
 
-                row.operator(API_OT_GOTO_Module.bl_idname, text=str(
-                    entry), emboss=True).info = f'{i} {j}'
+                row.operator(API_OT_GOTO_Module.bl_idname, text=str(entry),
+                             emboss=True).info = f'{i} {j}'
+    
+        return
+
+
+class APIBrowserAddonPreferences(AddonPreferences):
+    bl_idname = __name__
+
+    default_module: StringProperty(
+        name="Default Module",
+        description="Default module to display",
+        default="bpy",
+    )
+    columns: IntProperty(
+        name="Column Count",
+        description="Column count",
+        default=3,
+    )
+    history_size: IntProperty(
+        name="History Size",
+        description="Number of history items to keep",
+        default=10,
+    )
+
+    def draw(self, context):
+        layout = self.layout
+        layout.use_property_split = True
+        layout.use_property_decorate = False  # No animation.
+
+        col = layout.column(align=True)
+        col.prop(self, "default_module")
+        col.prop(self, "columns")
+        col.prop(self, "history_size")
 
 
 #########################################################################################
+# PROPERTIES
+#########################################################################################
 
-from bpy.props import StringProperty, BoolProperty, PointerProperty
-
-class API_Props(bpy.types.PropertyGroup):
+class API_Props(PropertyGroup):
 
     path: StringProperty(
         name="Path",
         description="API path",
-        default=DEFAULT_MODULE
+        default="bpy"
     )
     filter: StringProperty(
         name="Filter",
@@ -357,11 +486,14 @@ class API_Props(bpy.types.PropertyGroup):
 
 
 classes = (
+    API_MT_History_Menu,
+    API_OT_History,
     API_OT_GOTO_Module,
     API_OT_Reload_Module,
     API_OT_Copy_Text,
     API_OT_Api_Info,
     API_PT_Browser,
+    APIBrowserAddonPreferences,
     API_Props,
 )
 
